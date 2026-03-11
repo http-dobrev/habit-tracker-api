4d6b1c29183feb7e09ba4a832d83ac79905fb1d3
Dobrev,Konstantin K.N. <529830@student.fontys.nl>
2026-03-11 16:26:29 +0100

    fixed directory for controller package

--- src/main/java/com/konstantin/habittracker/controller/TestController.java
+++ src/main/java/com/konstantin/habittracker/controller/TestController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.konstantin.habittracker.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
