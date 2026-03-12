86ab1fda6a043758766e60f1832e42ef1a72a4d7
Dobrev,Konstantin K.N. <529830@student.fontys.nl>
2026-03-12 13:51:12 +0100

    Added 2 helper classes for testing db connection

---
+++ src/main/java/com/konstantin/habittracker/TestEntity.java
@@ -0,0 +1,25 @@
+package com.konstantin.habittracker;
+
+import jakarta.persistence.*;
+
+@Entity
+public class TestEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
