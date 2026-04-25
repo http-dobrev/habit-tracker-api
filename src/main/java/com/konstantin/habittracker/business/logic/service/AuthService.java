c4ce2544e98b1228f6b9938a57aef7df0e4472e7
konstantin <konstantindobrev576@gmail.com>
2026-04-25 14:15:23 +0200

    removed RegisterValidationService.java, added validation in RegisterRequest itself, added implementation for AuthService.java, initialized AuthService in AuthController

--- src/main/java/com/konstantin/habittracker/business/logic/service/AuthService.java
+++ src/main/java/com/konstantin/habittracker/business/logic/service/AuthService.java
@@ -9,4 +9,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthService {
 
+    public AuthResponse register(RegisterRequest request) {
+        if (request == null) {
+            throw new InvalidRequestException("Invalid request");
+        }
+
+        if (request.getEmail().equalsIgnoreCase("john@example.com")) {
+            throw new EmailAlreadyExistsException("Email already exists");
+        }
+
+        return new AuthResponse(
+                "jwt-token-here",
+                3600,
+                "User successfully registered"
+        );
+    }
 }
