d7789f2174989352829a63ece25c687038686db3
konstantin <konstantindobrev576@gmail.com>
2026-05-19 13:04:56 +0200

    fix: changed HabitCompletionController endpoint to "habit-completions" from just "completions"

--- src/main/java/com/konstantin/habittracker/controller/HabitCompletionController.java
+++ src/main/java/com/konstantin/habittracker/controller/HabitCompletionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/completions")
+@RequestMapping("/api/habit-completions")
 public class HabitCompletionController {
 
     private final HabitCompletionService habitCompletionService;
